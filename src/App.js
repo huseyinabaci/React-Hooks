8295bb48fe75ca622ecb9f6cd3cf1e54a7b34251
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-10-08 22:16:05 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
-import { useReducer } from "react";
-import Calculate from "./Calculate";
-
-export const NumberContext = React.createContext();
-
-const initialValue = 0;
-
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialValue;
-    default:
-      return state;
-  }
-};
+import { useState, useMemo } from 'react';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialValue);
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunc(number);
+  },[number])
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF' : '#333',
+  };
+
   return (
     <div className="App">
-      <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
-        <Calculate />
-      </NumberContext.Provider>
+      <>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(parseInt(e.target.value))}
+        />
+        <button onClick={() => setDark((prevDark) => !prevDark)}>
+          Temayı Değiştir
+        </button>
+        <div style={theme}>{doubleNumber}</div>
+      </>
     </div>
   );
+}
+
+function slowFunc(num) {
+  console.log('Fonksiyon çağrıldı');
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
 }
 
 export default App;
