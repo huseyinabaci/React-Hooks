74fc8c71fe6663b5519973c55ad3fd57158d0709
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-10-12 20:32:36 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import { useState, useTransition } from 'react';
+import { useState } from 'react';
+import Characters from './Characters';
 
 function App() {
   const [input, setInput] = useState('');
-  const [myList, setMyList] = useState([]);
-  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setInput(e.target.value);
-
-    startTransition(() => {
-      const myArray = [];
-      for (let i = 0; i < 2000; i++) {
-        myArray.push(e.target.value);
-      }
-      setMyList(myArray);
-    });
   };
 
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
-
-      {isPending
-        ? 'Yükleniyor...'
-        : myList.map((item, index) => {
-            return <div key={index}>{item}</div>;
-          })}
+      <Characters input={input} />
     </>
   );
 }
